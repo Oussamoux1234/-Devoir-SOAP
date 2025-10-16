b656822ab94499ac8ea28499305d0922e5b4b573
Oussama Essalmani <98963291+Oussamoux1234@users.noreply.github.com>
2025-10-16 23:31:27 +0100

    Update listEtudiants to return new student data

--- src/main/java/Service/EtudiantWService.java
+++ src/main/java/Service/EtudiantWService.java
@@ -12,9 +12,9 @@ public class EtudiantWService {
     @WebMethod(operationName = "listStudents")
     public List<Student> listEtudiants(){
         return List.of(
-                new Student(1, "ET_Nom1", "ET_Prénom1", Genre.Homme),
-                new Student(2, "ET_Nom2", "ET_Prénom2", Genre.Femme),
-                new Student(3, "ET_Nom3", "ET_Prénom3", Genre.Homme)
+                new Student(1, "ESSAL", "Prénom1", Genre.Homme),
+                new Student(2, "Essal2", "Prénom2", Genre.Femme),
+                new Student(3, "Eess3", "Prénom3", Genre.Homme)
         );
     }
 }
